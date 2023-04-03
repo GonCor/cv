06541000ebb2009b451cbd123d836c00481dd2a1
GonCor <cortezriquelmecl@gmail.com>
2023-04-03 01:21:51 -0300

    First upload to repository

---
+++ profile/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AsideComponent } from './components/aside/aside.component';
+import { MainComponent } from './components/main/main.component';
+import { SectionComponent } from './components/section/section.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    AsideComponent,
+    MainComponent,
+    SectionComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
